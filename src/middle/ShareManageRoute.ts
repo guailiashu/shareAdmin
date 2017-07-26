0f40e5cfcd9b0a42c5c8d387385df962365ec315
caoyun22 <1246979829@qq.com>
2017-07-26 11:22:18 +0800

    11

--- src/middle/ShareManageRoute.ts
+++ src/middle/ShareManageRoute.ts
@@ -45,6 +45,7 @@ export class ShareManageRoute extends Route.BaseRoute implements Route.IRoute{
         let todaySignupCount = await this.db.userModel.find().where('createDt').gt(todayStart).lt(todayEnd).count().exec();
         let todayTaskRecords = await this.db.taskRecordModel.find().where('createDt').gt(yesStart).lt(todayEnd).exec();
         let activeUsers = [];
+        
         todayTaskRecords.forEach(record=>{
             //console.log(record.shareDetail[0].user);
             if(activeUsers.includes(record.shareDetail[0].user)){
