04b50d096a4cd3b0bc2790bd59bbd0599e753314
caoyun22 <1246979829@qq.com>
2017-07-28 10:17:30 +0800

    11

--- src/models/TaskTag.ts
+++ src/models/TaskTag.ts
@@ -3,14 +3,9 @@ import mongoose = require('mongoose');
 
 var taskTagSchema = new mongoose.Schema({
     name: String,
-<<<<<<< HEAD
     sort: { type: Number, default: 0 },
-=======
->>>>>>> da43ba8b333dcc25d6bb13c9739e41d25ca9588e
     createDt: { type: Date, default: Date.now }
 });
-
-
 
 
 export var taskTagModel = mongoose.model('TaskTag', taskTagSchema);
